bb4a04c7eb030e3da26603cc8a1b31bd7dedeaaa
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-08-06 04:52:21 +0900

    何か変わってなかったのでコミットしなおし。

--- SlotMadomagiSettingDiscrimination/MainForm.Designer.cs
+++ SlotMadomagiSettingDiscrimination/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SlotMadomagiSettingDiscrimination
 {
-	partial class Form1
+	partial class MainForm
 	{
 		/// <summary>
 		/// 必要なデザイナー変数です。
@@ -28,10 +28,17 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.SuspendLayout();
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.Name = "MainForm";
+			this.Text = "初代まどマギ設定判別ツール";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
